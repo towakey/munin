4fd051e0e6526a0669bb2d201855dbbc9eda4453
towakey <kyosgame@gmail.com>
2022-10-03 01:47:49 +0900

    内容修正

--- backend/app/Http/Controllers/Api/Auth/LoginController.php
+++ backend/app/Http/Controllers/Api/Auth/LoginController.php
@@ -1,10 +1,32 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api\Auth;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+
+use Hash;
+use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
 {
     //
+    public function login(Request $request)
+    {
+        $email = $request->email;
+        $password = $request->password;
+
+        $user = User::where('email', $email)->first();
+
+        if(! $user || ! Hash::check($password, $user->password))
+        {
+            throw ValidationException::withMessages([
+                'email' => ['Not Mail or Not Password'],
+            ]);
+        }
+
+        $token = $user->createToken('token')->plainTextToken;
+
+        return response()->json(compact('token'), 200);
+    }
 }
