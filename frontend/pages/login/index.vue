3d14331640d7d842773300e10aa1f037718e7217
towakey <kyosgame@gmail.com>
2022-10-05 00:10:33 +0900

    login画面一旦コミット

---
+++ frontend/pages/login/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+      <v-text-field label="email" v-model="email"></v-text-field>
+      <v-text-field label="password" v-model="password"></v-text-field>
+      <v-btn @click="login" color="blue" outlined>LOGIN</v-btn>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    login() {
+      console.log("login methods");
+      this.$axios
+        .$post("/api/login", {
+          email: this.email,
+          password: this.password,
+        })
+        .then((response) => {
+          console.log(response);
+        });
+    },
+  },
+};
+</script>
