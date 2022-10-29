06a1bf9d975bd3d8b4289945cca8855f05401086
towakey <kyosgame@gmail.com>
2022-10-29 17:51:12 +0900

    timelineの表示を最新順に

--- backend/app/Http/Controllers/Api/NoteController.php
+++ backend/app/Http/Controllers/Api/NoteController.php
@@ -18,7 +18,7 @@ class NoteController extends Controller
     public function index()
     {
         //
-        $note=DB::table('notes')->get();
+        $note=DB::table('notes')->latest()->get();
         return $note;
     }
 
