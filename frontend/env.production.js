f44309e869809b9e6f73c862f82470fa5f333b42
towakey <kyosgame@gmail.com>
2022-11-05 00:51:52 +0900

    cross-env導入

---
+++ frontend/env.production.js
@@ -0,0 +1,7 @@
+module.exports = {
+    NODE_ENV: 'production',
+    API_URL: 'http://localhost/munin/backend/public/',
+    SERVER_URL: 'http://localhost/munin/backend/public/',
+    NODE_URL: 'http://localhost:3000/',
+    BASE_URL: '',
+}
